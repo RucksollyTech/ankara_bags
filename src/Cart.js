f56b1df266cc3531d52599142b22294f73cc20c7
Rucksolly <rucksolly.tech@gmail.com>
2024-02-13 09:49:29 +0100

    feb 13 4th commit

--- src/Cart.js
+++ src/Cart.js
@@ -15,11 +15,23 @@ const Cart = () => {
     return (
         <div className='cart'>
             <div className="cartContent">
+                <div className="detailsCarouselSm">
+                    <div className='py-5'>
+                        <button className='productButton'>
+                            Proceed to checkout
+                        </button>
+                        <div className='pt-3 text-center'>
+                            <Link className='anchor' to={"/"}>
+                                Continue shopping
+                            </Link>
+                        </div>
+                    </div>
+                </div>
                 <div className="twoEquo centerY">
                     <div className="header2">
                         My Shopping Cart <span>(3)</span>
                     </div>
-                    <div>
+                    <div className='detailsCarouselLg'>
                         <Link className='anchor' to={"/"}>
                             Continue shopping
                         </Link>
@@ -28,21 +40,27 @@ const Cart = () => {
                 </div>
                 <div className='cartDetailsContainer'>
                     <div className='shopLeft'>
-                        <div className='theContent img relative' onClick={()=>zoomer(["/Images/bag.avif","/Images/bag2.avif"])}>
-                            <img src="/Images/bag.avif" alt="bag" />
+                        <div className='theContent relative' onClick={()=>zoomer(["/Images/bag.avif","/Images/bag2.avif"])}>
+                            <img className='img' src="/Images/bag.avif" alt="bag" />
                             <img className='zoomIcon' width="20" height="20" src="https://img.icons8.com/external-icongeek26-outline-icongeek26/20/external-zoom-in-graphic-design-icongeek26-outline-icongeek26.png" alt="external-zoom-in-graphic-design-icongeek26-outline-icongeek26"/>
                         </div>
                     </div>
                     <div className='border_left'>
-                        <div className='border_bottom padderCart'>
-                            <div>
-                                <small>
-                                    BG1001
-                                </small>
+                        <div className='border_bottom padderCart lilSmImgContainer'>
+                            <div className='lilSmImg relative' onClick={()=>zoomer(["/Images/bag.avif","/Images/bag2.avif"])}>
+                                <img className='img' src="/Images/bag.avif" alt="bag" />
+                                <img className='zoomIcon' width="20" height="20" src="https://img.icons8.com/external-icongeek26-outline-icongeek26/20/external-zoom-in-graphic-design-icongeek26-outline-icongeek26.png" alt="external-zoom-in-graphic-design-icongeek26-outline-icongeek26"/>
                             </div>
-                            <div className='ppadName'>
-                                <div className="productName">
-                                    Bag Name and Names
+                            <div className='pdSmSc'>
+                                <div>
+                                    <small>
+                                        BG1001
+                                    </small>
+                                </div>
+                                <div className='ppadName'>
+                                    <div className="productName">
+                                        Bag Name and Names
+                                    </div>
                                 </div>
                             </div>
                         </div>
@@ -82,72 +100,6 @@ const Cart = () => {
                         </div>
                         <div className='divideByTwo'>
                             <div className='text-center xx pointer'  onClick={togglerSideBar}>
-                                <img width="15" height="15" src="https://img.icons8.com/fluency-systems-regular/15/visible--v1.png" alt="visible--v1"/>
-                                <span className='pl_1'>View Details</span>
-                            </div>
-                            <div className='text-center pointer'>
-                                <img width="15" height="15" src="https://img.icons8.com/ios/15/cancel.png" alt="cancel"/>
-                                <span className='pl_1'>Remove</span>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className='cartDetailsContainer'>
-                    <div className='shopLeft'>
-                        <div className='theContent img relative' onClick={()=>zoomer(["/Images/bag.avif","/Images/bag2.avif"])}>
-                            <img src="/Images/bag.avif" alt="bag" />
-                            <img className='zoomIcon' width="20" height="20" src="https://img.icons8.com/external-icongeek26-outline-icongeek26/20/external-zoom-in-graphic-design-icongeek26-outline-icongeek26.png" alt="external-zoom-in-graphic-design-icongeek26-outline-icongeek26"/>
-                        </div>
-                    </div>
-                    <div className='border_left'>
-                        <div className='border_bottom padderCart'>
-                            <div>
-                                <small>
-                                    BG1001
-                                </small>
-                            </div>
-                            <div className='ppadName'>
-                                <div className="productName">
-                                    Bag Name and Names
-                                </div>
-                            </div>
-                        </div>
-                        <div className="padderCart">
-                            <div className="twoEquo">
-                                <span>
-                                    Color
-                                </span>
-                                <span>
-                                    Monogram
-                                </span>
-                            </div>
-                            <div className="twoEquo pt-1">
-                                <span>
-                                    Color
-                                </span>
-                                <span>
-                                    Monogram
-                                </span>
-                            </div>
-                            
-                        </div>
-                        <div className="heightManager" />
-                        <div className="padderCart border_bottom">
-                            <div className="twoEquo centerY">
-                                <div className='sel'>
-                                    <select>
-                                        <option value="">
-                                            2
-                                        </option>
-                                    </select>
-                                </div>
-                                <span>
-                                    $4,740.00
-                                </span>
-                            </div>
-                        </div>
-                        <div className='divideByTwo'>
-                            <div className='text-center xx pointer' onClick={togglerSideBar}>
                                 <img width="15" height="15" src="https://img.icons8.com/fluency-systems-regular/15/visible--v1.png" alt="visible--v1"/>
                                 <span className='pl_1'>View Details</span>
                             </div>
@@ -250,11 +202,12 @@ const Cart = () => {
                         <div>
                             Lorem ipsum dolor sit amet consectetur.
                         </div>
-                        <div className="sticky-bottom bg-white">
-                            <button className='productButton'>
-                                View Product Page
-                            </button>
-                        </div>
+                        
+                    </div>
+                    <div className="sticky-bottom bg-white">
+                        <button className='productButton'>
+                            View Product Page
+                        </button>
                     </div>
                 </div>
             </div>
