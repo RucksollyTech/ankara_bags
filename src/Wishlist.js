de761f222170e98137f1531e3870dc1f67f9a0ab
Rucksolly <rucksolly.tech@gmail.com>
2024-02-13 18:49:26 +0100

    feb 13 10th commit

--- src/Wishlist.js
+++ src/Wishlist.js
@@ -71,7 +71,180 @@ const Wishlist = () => {
                     </div>
                     <img onClick={()=>setShow(true)} className='cancelWishList' width="20" height="20" src="https://img.icons8.com/fluency-systems-filled/20/delete-sign.png" alt="delete-sign"/>
                 </div>
-                
+                <div className='wishListContent relative'>
+                    <div className="theContent" onClick={toggler}>
+                        <img className='pointer' src="/Images/bag.avif" alt="Bag" />
+                        <div className="wishListDetailDisplay pointer" onClick={removeToggle}>
+                            <div>
+                                <small>
+                                    BG1001
+                                </small>
+                            </div>
+                            <div className='ppadName'>
+                                <div className="productName font_17">
+                                    Bag Name and Names
+                                </div>
+                            </div>
+                            <div className="pt-1 font_12">
+                                <span>
+                                    Color
+                                </span>
+                                <span>
+                                    Monogram
+                                </span>
+                            </div>
+                            <div className=" pt-1 font_12">
+                                <span>
+                                    Material
+                                </span>
+                                <span>
+                                    Monogram
+                                </span>
+                            </div>
+                            <div className="font_12 py-3">
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum, delectus!
+                            </div>
+                            <div className='text-dark py-3 xd'>
+                                <button className='productButton'>
+                                    Place in cart
+                                </button>
+                                <div className='pt-2'>
+                                    <button className='productButton xx'>
+                                        <span className="mr-1">
+                                            <img width="15" height="15" src="https://img.icons8.com/material-rounded/15/plus-math--v1.png" alt="plus-math--v1"/>
+                                        </span>Full Details
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="bagWords bg-white">
+                            <div>
+                                Bag Name and Names
+                            </div>
+                            <div>
+                                $2,200.00
+                            </div>
+                            
+                        </div>
+                    </div>
+                    <img onClick={()=>setShow(true)} className='cancelWishList' width="20" height="20" src="https://img.icons8.com/fluency-systems-filled/20/delete-sign.png" alt="delete-sign"/>
+                </div>
+                <div className='wishListContent relative'>
+                    <div className="theContent" onClick={toggler}>
+                        <img className='pointer' src="/Images/bag.avif" alt="Bag" />
+                        <div className="wishListDetailDisplay pointer" onClick={removeToggle}>
+                            <div>
+                                <small>
+                                    BG1001
+                                </small>
+                            </div>
+                            <div className='ppadName'>
+                                <div className="productName font_17">
+                                    Bag Name and Names
+                                </div>
+                            </div>
+                            <div className="pt-1 font_12">
+                                <span>
+                                    Color
+                                </span>
+                                <span>
+                                    Monogram
+                                </span>
+                            </div>
+                            <div className=" pt-1 font_12">
+                                <span>
+                                    Material
+                                </span>
+                                <span>
+                                    Monogram
+                                </span>
+                            </div>
+                            <div className="font_12 py-3">
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum, delectus!
+                            </div>
+                            <div className='text-dark py-3 xd'>
+                                <button className='productButton'>
+                                    Place in cart
+                                </button>
+                                <div className='pt-2'>
+                                    <button className='productButton xx'>
+                                        <span className="mr-1">
+                                            <img width="15" height="15" src="https://img.icons8.com/material-rounded/15/plus-math--v1.png" alt="plus-math--v1"/>
+                                        </span>Full Details
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="bagWords bg-white">
+                            <div>
+                                Bag Name and Names
+                            </div>
+                            <div>
+                                $2,200.00
+                            </div>
+                            
+                        </div>
+                    </div>
+                    <img onClick={()=>setShow(true)} className='cancelWishList' width="20" height="20" src="https://img.icons8.com/fluency-systems-filled/20/delete-sign.png" alt="delete-sign"/>
+                </div>
+                <div className='wishListContent relative'>
+                    <div className="theContent" onClick={toggler}>
+                        <img className='pointer' src="/Images/bag.avif" alt="Bag" />
+                        <div className="wishListDetailDisplay pointer" onClick={removeToggle}>
+                            <div>
+                                <small>
+                                    BG1001
+                                </small>
+                            </div>
+                            <div className='ppadName'>
+                                <div className="productName font_17">
+                                    Bag Name and Names
+                                </div>
+                            </div>
+                            <div className="pt-1 font_12">
+                                <span>
+                                    Color
+                                </span>
+                                <span>
+                                    Monogram
+                                </span>
+                            </div>
+                            <div className=" pt-1 font_12">
+                                <span>
+                                    Material
+                                </span>
+                                <span>
+                                    Monogram
+                                </span>
+                            </div>
+                            <div className="font_12 py-3">
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum, delectus!
+                            </div>
+                            <div className='text-dark py-3 xd'>
+                                <button className='productButton'>
+                                    Place in cart
+                                </button>
+                                <div className='pt-2'>
+                                    <button className='productButton xx'>
+                                        <span className="mr-1">
+                                            <img width="15" height="15" src="https://img.icons8.com/material-rounded/15/plus-math--v1.png" alt="plus-math--v1"/>
+                                        </span>Full Details
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="bagWords bg-white">
+                            <div>
+                                Bag Name and Names
+                            </div>
+                            <div>
+                                $2,200.00
+                            </div>
+                            
+                        </div>
+                    </div>
+                    <img onClick={()=>setShow(true)} className='cancelWishList' width="20" height="20" src="https://img.icons8.com/fluency-systems-filled/20/delete-sign.png" alt="delete-sign"/>
+                </div>
             </div>
             <QuestionModal 
                 show={show}
